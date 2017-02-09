cface505b2908c7338b9188a935d3bab92cec295
Alexandra RIBEIRO <aribeiro@e3r3p10.42.fr>
2017-02-09 15:03:04 +0100

    backup

--- memory.h
+++ memory.h
@@ -6,7 +6,7 @@
 /*   By: aribeiro <aribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 13:01:38 by aribeiro          #+#    #+#             */
-/*   Updated: 2017/02/08 14:41:00 by aribeiro         ###   ########.fr       */
+/*   Updated: 2017/02/09 14:23:36 by aribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,37 @@
 
 # include <unistd.h>
 # include <stdio.h>
-# include <stdlib.h>
+//# include <stdlib.h>
 # include <sys/mman.h>
 # include <sys/resource.h>
 
-typedef struct	s_page
+typedef struct		s_page
 {
-	int		count; // indique le nombre de blocks restants
-
-
-}				t_page;
+	int				flag;
+	int				req_size;
+	struct s_page	*next;
+	struct s_page	*previous;
+}					t_page;
 
 /*_______ MY MALLOC __________________________________________________________*/
-// void	*malloc(size_t size);
+// void		*malloc(size_t size);
 
 /*_______ MY FREE ____________________________________________________________*/
-// void	free(void *ptr);
+// void		free(void *ptr);
 
 /*_______ MY REALLOC _________________________________________________________*/
-// void	*realloc(void *ptr, size_t size);
+// void		*realloc(void *ptr, size_t size);
+
+/*_______ Show allocation memory _____________________________________________*/
+// void		show_alloc_mem();
 
 /*_______ LIBFT ______________________________________________________________*/
-void	ft_bzero(void *s, size_t n);
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void	ft_memdel(void **ap);
+void				ft_bzero(void *s, size_t n);
+void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void				ft_memdel(void **ap);
 
 /*_______ BONUS ______________________________________________________________*/
 // free_all (libere tous les pointeurs)
-// free_all_secure (libere les pointeus et reinitialise la memore a 0 avec bzero)
+// free_all_secure (libere les ptr et reinitialise la memoire a 0 avec bzero)
 
 #endif
--- my_malloc.c
+++ my_malloc.c
@@ -6,10 +6,58 @@
 /*   By: aribeiro <aribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 13:52:10 by aribeiro          #+#    #+#             */
-/*   Updated: 2017/02/08 13:53:09 by aribeiro         ###   ########.fr       */
+/*   Updated: 2017/02/09 14:56:02 by aribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "memory.h"
 
-// void	*malloc(size_t size);
+void	*tiny(t_page **tiny, size_t size)
+{
+	void	*block;
+	if (*tiny == NULL)
+	{
+		block = mmap(0, (128*100), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+	}
+	else
+	{
+
+	}
+	return ();
+}
+
+void	*small(t_page **small, size_t size)
+{
+	if (*small == NULL)
+
+	else
+	return ();
+}
+
+void	*large(t_page **large. size_t size)
+{
+	if (*large == NULL)
+
+	else
+	return ();
+}
+
+//changer le nom de la fonction
+void	*my_malloc(size_t size)
+{
+	t_page *tiny;
+	t_page *small;
+	t_page *large;
+
+	tiny = NULL;
+	small = NULL;
+	large = NULL;
+	if (size <= 0)
+		return (NULL);
+	else if  (size < 100)
+		return (tiny(&tiny, size));
+	else if (size < 4000)
+		return (small(&small, size));
+	else
+	 	return (large(&large, size));
+}
