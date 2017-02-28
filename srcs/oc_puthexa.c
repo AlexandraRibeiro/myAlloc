30c8755af641465e8c0bf313d216dc6246304623
Alexandra Ribeiro <aribeiro@student.42.fr>
2017-02-28 12:31:22 +0100

    + oc_puthexa

---
+++ srcs/oc_puthexa.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   oc_puthexa.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aribeiro <aribeiro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/28 12:27:07 by aribeiro          #+#    #+#             */
+/*   Updated: 2017/02/28 12:30:51 by aribeiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "memory.h"
+
+void 		oc_puthexa(size_t addr)
+{
+	size_t i;
+
+	i = 0;
+	if (addr)
+	{
+		i = dec / 16;
+		oc_puthexa(i);
+		if (addr % 16 < 10)
+			oc_putchar_fd('0' + addr % 16, 1);
+		else
+			oc_putchar_fd('a' + (addr % 16) - 10, 1);
+	}
+	else
+		oc_putstr_fd("0x", 1);
+	return ;
+}
